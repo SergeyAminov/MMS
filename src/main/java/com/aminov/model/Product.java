2d670c7694538e8b84f50666119d7ea48d615dca
SergeyAminov <aminov_yar@mail.ru>
2020-07-11 17:41:41 +0300

    set HibernateConfig, WebConfig, AppInitializer and Product entity

---
+++ src/main/java/com/aminov/model/Product.java
@@ -0,0 +1,93 @@
+package com.aminov.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "price")
+    private double price;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "weight")
+    private double weight;
+
+    @Column(name = "size")
+    private double size;
+
+    @Column(name = "count")
+    private int count;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public String toString(){
+        return this.title;
+    }
+}
